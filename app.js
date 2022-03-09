950306609974ce55322fa37953ff56131e3de20f
ronya78@gmail.com <ronya78@gmail.com>
2022-03-09 16:48:30 +0200

    Add a new page with name Work.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];//<- "New Item"
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -23,14 +24,23 @@ app.get("/", function(req, res){
   };
 
   let day = today.toLocaleDateString("en-US", options);
-  res.render("list", {kindOfDay: day, newListItems: items});
+  res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req,res){
   let item = req.body.newItem;
 
-  items.push(item);
-  res.redirect("/");
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
   // var currentDay = today.getDay();
